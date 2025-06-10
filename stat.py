e40b3750f6484f28cf1f32e25f15f533c24d1bb8
HxLyn3 <linhx@lamda.nju.edu.cn>
2025-06-10 14:29:31 +0800

    add performance statistics

---
+++ stat.py
@@ -0,0 +1,41 @@
+import os
+import gym
+import d4rl
+import json
+import argparse
+
+import numpy as np
+
+def get_args():
+    parser = argparse.ArgumentParser(description="Model as Simulator")
+
+    # environment settings
+    parser.add_argument("--env", type=str, default="d4rl")
+    parser.add_argument("--env-name", type=str, default="halfcheetah-medium-v2")
+    parser.add_argument("--load-label", type=str, default="sadm-sac")
+
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = get_args()
+    res_path = f"./result/{args.env}/{args.env_name}/{args.load_label}"
+    
+    scores = []
+    for load_time in os.listdir(res_path):
+        rec_path = f"{res_path}/{load_time}/record"
+        for fname in os.listdir(rec_path):
+            if fname.split('.')[1] != "txt": continue
+            if "model" in fname: continue
+            f_path = os.path.join(rec_path, fname)
+            with open(f_path, "r") as f:
+                record = json.load(f)
+                if "score_mean" in record:
+                    scores.append(record["score_mean"][-1])
+                    
+    print(f"task: {args.env_name}@{args.env}")
+    print(f"score mean: {np.mean(scores)}")
+    print(f"score std: {np.std(scores)}")
+
+if __name__ == "__main__":
+    main()
